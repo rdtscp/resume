8bed01e824e3578d047a36ef8349c2a25a5f4db2
Alexander Wilson <acwilson96@gmail.com>
2018-06-01 16:08:28 +0100

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+document.title = "alex@wilson: ~$";
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
