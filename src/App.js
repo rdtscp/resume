cab0056c2d8ff5a85ef6d1559361a67021666d17
Alexander Wilson <acwilson96@gmail.com>
2018-06-01 16:16:49 +0100

    Boilerplate

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+
+  constructor(props) {
+    this.state = {
+      compiled: false
+    };
+  }
+
   render() {
-    return (
-      <div className="App">
-        Interactive CV
-      </div>
-    );
+    if (this.state.compiled) {
+      return (
+        <div className="App">
+          Compiled Resume
+        </div>
+      );
+    }
+    else {
+      return (
+        <div className="App">
+          Code Resume
+        </div>
+      );
+    }
   }
 }
 
